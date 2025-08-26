ffee98a69e506be5516af5d686ed5e77a64dbcea
FARHI ALI <farhiali0000@gmail.com>
2025-08-26 09:15:42 +0100

    04-authentication ui

---
+++ gomeet/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+
+import { SignInView } from "@/models/auth/ui/views/sign-in";
+
+
+const page = () => { 
+   
+return<SignInView/>
+
+}
+export default page;
--- gomeet/src/lib/auth.ts
+++ gomeet/src/lib/auth.ts
@@ -1,11 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
- 
+import { schema } from "@/db/schema"; // <-- make sure you export schema
+
 export const auth = betterAuth({
-emailAndPassword: {
-    enabled: true, 
+  emailAndPassword: {
+    enabled: true,
   },
- database: drizzleAdapter(db, {
-        provider: "pg",}),
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema, // <-- this is required
+  }),
 });
