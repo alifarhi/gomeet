09cac4f7224b28e1b435943518123c69a833c009
FARHI ALI <farhiali0000@gmail.com>
2025-09-19 12:29:22 +0100

    20-meeting_id_page

--- gomeet/src/app/(dashboard)/meetings/[meetingId]/page.tsx
+++ gomeet/src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -1,9 +1,41 @@
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { MeetingIdView } from "@/models/meetings/ui/views/meeting_id_view";
 
-const Page = () => {
+
+interface Props{
+  params:Promise<{
+    meetingId:string;
+  }>
+}
+
+const Page = async({params}:Props) => {
+const {meetingId}=await params;
+const session=await auth.api.getSession({
+  headers:await headers(),
+});
+if(!session){
+  redirect("/sign-in")
+};
+
+const queryClient=getQueryClient();
+void queryClient.prefetchQuery(
+  trpc.meetings.getOne.queryOptions({id:meetingId})
+)
+
   return (
-    <div>
-        meetings id page
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>todo</p>}>
+         <ErrorBoundary fallback={<p>todo</p>}>
+          <MeetingIdView meetingId={meetingId} />
+         </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
   );
 };
  export default Page;
