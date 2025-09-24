f458e87375cd18e5a9866e9e7efe876e8f785600
FARHI ALI <farhiali0000@gmail.com>
2025-09-24 23:23:45 +0100

    cleand page

--- gomeet/src/app/(dashboard)/agents/[agentId]/page.tsx
+++ gomeet/src/app/(dashboard)/agents/[agentId]/page.tsx
@@ -1,33 +1,28 @@
-
-
 import { AgentIdView, AgentIdViewError, AgentIdViewLoading } from "@/models/agents/ui/views/agent-id-view";
 import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary, queryOptions } from "@tanstack/react-query";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 
-
-interface Props{
-    params:Promise<{agentId: string}>
+interface Props {
+  params: { agentId: string };
 }
 
+export default async function AgentPage({ params }: Props) {
+  const { agentId } = params;
+  const queryClient = getQueryClient();
 
-export const page = async({params}:Props) => {
-    const {agentId}=await params;
-    const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(
-        trpc.agents.getOne.queryOptions({id:agentId})
-    );
+  await queryClient.prefetchQuery(
+    trpc.agents.getOne.queryOptions({ id: agentId })
+  );
 
   return (
-<HydrationBoundary state={dehydrate(queryClient)}>
-  <Suspense fallback={<AgentIdViewLoading/>} >
-    <ErrorBoundary  fallback={<AgentIdViewError/>}>
-         <AgentIdView agentId={agentId}/>
-    </ErrorBoundary>
-  </Suspense>
-</HydrationBoundary>
-)
-};
-
-export default page;
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<AgentIdViewLoading />}>
+        <ErrorBoundary fallback={<AgentIdViewError />}>
+          <AgentIdView agentId={agentId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+}
